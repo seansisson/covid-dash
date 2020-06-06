0cfccb1409aa62345164c5c1f29855d46b54842a
Sean Sisson <sisson@pdx.edu>
2020-06-05 20:58:39 -0700

    Add Pie chart screen

---
+++ screens/PieChartsScreen.js
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { Text, StyleSheet, Dimensions } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { PieChart } from 'react-native-chart-kit'
+
+export default function PieChartsScreen() {
+  // Create state parameters for data parameters
+  // Also create constants for API URL and headers, etc
+  const [recovered, setRecovered] = useState(0);
+  const [confirmed, setConfirmed] = useState(0);
+  const [deaths, setDeaths] = useState(0);
+  const url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total";
+  const options = {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+      "x-rapidapi-key": ""
+    }
+  }
+
+  // Call API and set state parameters with appropriate data
+  fetch(url.concat('?country=USA'), options)
+  .then(response => response.json())
+  .then(responseJson => {
+    setRecovered(responseJson['data'].recovered);
+    setConfirmed(responseJson['data'].confirmed);
+    setDeaths(responseJson['data'].deaths);
+  });
+
+  // Initialize final pie chart data
+  var pieData = [
+    {
+      name: 'Recovered',
+      population: recovered,
+      color: 'green',
+    },
+    {
+      name: 'Confirmed',
+      population: confirmed,
+      color: 'Orange',
+    },
+    {
+      name: 'Deaths',
+      population: deaths,
+      color: 'red',
+    },
+  ];
+
+  // Return a scrollable pi chart with supplied pie chart data (pieData)
+  return (
+    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      <Text>Chart</Text>
+      <PieChart
+        data={pieData}
+        width={Dimensions.get('window').width}
+        height={220}
+        accessor="population"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          }
+        }}
+        absolute
+      />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  contentContainer: {
+    paddingTop: 15,
+  },
+  optionIconContainer: {
+    marginRight: 12,
+  },
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: 0,
+    borderColor: '#ededed',
+  },
+  lastOption: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  optionText: {
+    fontSize: 15,
+    alignSelf: 'flex-start',
+    marginTop: 1,
+  },
+});
